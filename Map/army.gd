5c5f0aceb57db2b7cea2645484b5551267a5f818
Lucas Del Piero <lucasdelpiero98@gmail.com>
2023-04-27 02:58:11 -0300

    Pathfinding fixed and working perfectly

--- Map/army.gd
+++ Map/army.gd
@@ -6,10 +6,17 @@ var path : PackedVector2Array = []
 const JUMP_VELOCITY = -400.0
 @onready var line = $Node/Line2D
 enum  { IDLE, MOVING, FIGHTING }
-var state = IDLE
+var state = MOVING
 @export_range( 10, 10000, 1) var SPEED = 500
 var test = true
-var starting_point : Vector2
+var starting_point : Vector2 # Were the army starts traveling from one point to other
+
+# The three next points are important to avoid bugs and make sure that the army always travel from one point
+# to the other, avoiding changing direction in the middle of the travel between points to a new point and also
+# fixes that once a new path is generated it avoid going through the origin point again if it was already passed by
+var next_point : Vector2 # Next point that activates once the closest point is reached and deleted
+var first_point : Vector2 # First point in the path
+var second_point : Vector2 # Second point in the path
 
 signal get_pathfinding(army ,current_position)
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -19,19 +26,8 @@ func _input(event):
 #	if event is InputEventMouseButton:
 	if Input.is_action_just_pressed("Click_Left"):
 #		print("Mouse Click/Unclick at: ", event.position)
-		if own_map != null:
-			var from = own_map.get_closest_point(self.global_position)
-			var to = own_map.get_closest_point(event.position)
-			path = own_map.get_point_path(from, to)
-#			print(path)
-			draw_path()
-			if starting_point == path[0]:
-				path.remove_at(0)
-			if path.size() > 0:
-				state = MOVING
-			else:
-				state = IDLE
-#		path = own_map.get_point_path()
+		get_pathing(event.position)
+
 
 func _physics_process(delta):
 #	draw_path()
@@ -69,10 +65,20 @@ func move(delta):
 #		print("path " +str(path))
 		var closest_point = path[0]
 		var distance_to_point = global_position.distance_to(closest_point)
+		var destination = path[path.size() - 1]
+		$Node/closest.global_position = closest_point
+		if Input.is_action_just_pressed("Click_Right"):
+			global_position = closest_point
+			starting_point = closest_point
+		if Input.is_action_just_released("Click_Left"):
+			pass
+#			print(distance_to_point)
+#		$Node/distance.set_point_position(0, global_position)
+#		$Node/distance.set_point_position(1, )
 #		print("closest point " +str(closest_point))
 #		print("starting point " +str(starting_point))
 		
-		if (starting_point == closest_point) :
+		if (starting_point == closest_point) and (destination != starting_point):
 #			path.remove_at(0)
 			pass
 		
@@ -84,6 +90,29 @@ func move(delta):
 			global_position = path[0]
 			draw_path()
 			path.remove_at(0)
+			if path.size() > 0:
+				next_point = path[0]
+			
 #			print("delete")
-			print(path)
+#			print(path)
 			pass
+
+func get_pathing(destination):
+	if own_map != null:
+		var from = own_map.get_closest_point(self.global_position)
+		var to = own_map.get_closest_point(destination)
+		path = own_map.get_point_path(from, to)
+		draw_path()
+		
+		# If there are 2 or more than 2 points in the path the game has to check if the first and second points in the pathing
+		# correspond to the origin point and the next point to travel
+		# deleting the first point in the pathing avoids the army to go back to the origin point every time
+		# a new path is obtained and the origin is the closest point to start the pathing
+		if path.size() > 1 :
+			first_point = path[0]
+			second_point = path[1]
+			
+			if starting_point == path[0] and (second_point == next_point):
+				path.remove_at(0)
+	pass
+
