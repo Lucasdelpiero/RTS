5a696326df935b72af47daf5959393d4ff399ac5
Lucas Del Piero <lucasdelpiero98@gmail.com>
2023-05-21 02:56:14 -0300

    rotate unit instead of sprite when moving

--- Scripts/Battle/MoveComponent.gd
+++ Scripts/Battle/MoveComponent.gd
@@ -8,7 +8,6 @@ var path : PackedVector2Array = []
 var navigation_tilemap : TileMap = null : set = set_nav_map
 var nav_map = null
 @export_range(1,500, 1) var speed = 200
-@export var sprite : Sprite2D = null
 @onready var line = $Line2D
 var face_direction : float = 0.0
 
@@ -44,7 +43,7 @@ func _physics_process(delta):
 			next_point = path[0]
 	
 	unit.move_and_slide()
-	update_sprite_angle()
+	update_face_angle()
 
 func move_to(to, final_face_direction):
 	if unit == null or nav_map == null:
@@ -57,16 +56,14 @@ func move_to(to, final_face_direction):
 	face_direction = final_face_direction # angle of the unit once reaches its destination
 	pass
 
-func update_sprite_angle():
-	if sprite == null:
-		return
+func update_face_angle():
 	# Once the destination is reached it will face the desired angle
 	if path.size() <= 1:
-		sprite.rotation = lerp_angle(sprite.rotation, face_direction, 0.05)
+		unit.rotation = lerp_angle(unit.rotation, face_direction, 0.05)
 		return
 	# While moving it will face to the movement direction
 	var angle = unit.global_position.angle_to_point(next_point) + PI / 2
-	sprite.rotation = lerp_angle(sprite.rotation,angle, 0.05)
+	unit.rotation = lerp_angle(unit.rotation,angle, 0.05)
 
 func set_nav_map(value : TileMap):
 	nav_map = value.get_navigation_map(0)
