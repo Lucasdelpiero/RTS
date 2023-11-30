c1c08107007379acfa75e67b2d08d35d048f5aea
Lucasdelpiero <lucasdelpiero98@gmail.com>
2023-11-30 16:15:44 -0300

    Cleaning

--- Objects/Battle/task_group.gd
+++ Objects/Battle/task_group.gd
@@ -5,6 +5,6 @@ var group  = []
 var enemy_group_focused  = []
 var marker_to_follow = null
 
-enum Task {HOLD, ADVANCE, SKIRMISH, MELEE, FLEE}
+enum Task {FOLLOW_MARKER, HOLD, ADVANCE, SKIRMISH, MELEE, FLEE}
 
-var task = Task.HOLD
+var task = Task.FOLLOW_MARKER
