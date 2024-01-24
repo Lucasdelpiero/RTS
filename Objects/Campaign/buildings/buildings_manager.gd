8ed0326a8514a02040090b84bb391e3978f43e43
Lucasdelpiero <lucasdelpiero98@gmail.com>
2024-01-23 23:13:05 -0300

    buildings not built passed as resources to the manager

--- Objects/Campaign/buildings/buildings_manager.gd
+++ Objects/Campaign/buildings/buildings_manager.gd
@@ -15,15 +15,13 @@ var province_data : ProvinceData = ProvinceData.new() :
 		get_buildings_not_made(buildings)
 
 
-var building_types : Array[Building] = [
-	load("res://Objects/Campaign/buildings/building_farm.tres"),
-	load("res://Objects/Campaign/buildings/building_government.tres")
-]
+@export var building_types : Array[Building]  # Buildings that can be uilt
 
 func get_buildings_not_made(buildings : Array[Building]) -> Array[Building] :
 	 #Create a new resource to get the constants stored in a variable
 	var new_building : Building = Building.new()
 	var all_buildings_constants : Array = new_building.BUILDING_CONSTANTS
+	
 	
 	Globals.debug_update_label("constants", "constants: %s" %[all_buildings_constants])
 	
@@ -38,15 +36,24 @@ func get_buildings_not_made(buildings : Array[Building]) -> Array[Building] :
 	Globals.debug_update_label("built", "built: %s" %[already_built_constants])
 	
 	# Get buildings that hasnt been built in the city
-	var buildings_available_to_build : Array = []
+	var buildings_types_available_to_build : Array = []
 	for constant in all_buildings_constants:
 		if not constant in already_built_constants:
-			buildings_available_to_build.push_back(constant)
-
-	Globals.debug_update_label("available", "available: %s" % [buildings_available_to_build])
-
+			buildings_types_available_to_build.push_back(constant)
 	
-	return []
+	
+	Globals.debug_update_label("available", "available: %s" % [buildings_types_available_to_build])
+	
+	# Put the buildings that can be built in an array as a resource
+	var to_be_built : Array[Building] = []
+	for building in building_types:
+		if building.building_type in buildings_types_available_to_build:
+			to_be_built.push_back(building)
+	
+	Globals.debug_update_label("to_be_built", "to be built: %s" % [to_be_built])
+	
+	
+	return to_be_built
 
 # BUG loading these resources causes a crash 
 #const farm = building.new()
--- Objects/Campaign/buildings_UI.gd
+++ Objects/Campaign/buildings_UI.gd
@@ -5,14 +5,37 @@ extends Control
 @onready var buildings_container = %BuildingsContainer
 @onready var overview_container = %OverviewContainer
 @onready var ButtonBuilding  = preload("res://Objects/Campaign/button_building.tscn")
-@export var buildings_manager = BuildingsManager.new() # checks what can or can not be build
+@export var buildings_manager : BuildingsManager # checks what can or can not be build
 
 
-var province_data : ProvinceData = ProvinceData.new() :
+var province_data : ProvinceData = ProvinceData.new() : # Updated when clicked on a province
 	set(value):
 		province_data = value
 		buildings = value.buildings
 		buildings_manager.province_data = value
+		var to_be_built : Array[Building] = buildings_manager.get_buildings_not_made(buildings)
+		
+		
+		
+		if to_be_built.is_empty():
+			print("already built :)")
+		var new_buildings : Array[Building] = buildings.duplicate(true)
+		for building in to_be_built:
+			var button = ButtonBuilding.instantiate()
+			buildings_container.add_child(button)
+			
+			var new_building : Building 
+			new_building = building.duplicate(true) as Building
+			
+			
+			new_buildings.push_back(new_building)
+			
+			button.province_data = province_data
+			button.texture_normal = building.icon_normal
+			button.texture_hover = building.icon_hover
+		# TEST
+		
+		value.province.buildings_manager.buildings = new_buildings
 
 var buildings : Array[Building] :
 	set(value):
@@ -38,3 +61,4 @@ var buildings : Array[Building] :
 		
 		buildings = value
 
+# TEST to create buildings
