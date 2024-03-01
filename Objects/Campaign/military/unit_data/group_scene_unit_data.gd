a2fc431d51049bfdb53cd3386abf6cff6b9cd047
Lucasdelpiero <lucasdelpiero98@gmail.com>
2024-03-01 03:39:55 -0300

    Groups of units data based on culture

---
+++ Objects/Campaign/military/unit_data/group_scene_unit_data.gd
@@ -0,0 +1,7 @@
+class_name GroupSceneUnitData
+extends Resource
+
+# Stores a bunch of units based on the cultural group or nation it belongs to
+@export var ID : String = ""
+@export var array_scene_unit_data : Array[UnitData] = []
+
---
+++ Objects/Campaign/world/cultures.gd
@@ -0,0 +1,26 @@
+class_name Cultures
+extends Resource
+
+# Stores all the cultures as constants that can be accessed globally to avoid
+# mistakes writing
+
+enum list {
+	ITALIC,
+	CELT,
+	GREEK,
+	PHOENICIAN,
+	EGIPCIAN,
+	BRETON,
+	GERMANIC
+}
+
+
+const ITALIC := "italic"
+const CELT := "celt"
+const GREEK := "greek"
+const PHOENICIAN := "phoenician"
+const EGIPCIAN := "egipcian"
+const BRETON := "breton"
+const GERMANIC := "germanic"
+
+
