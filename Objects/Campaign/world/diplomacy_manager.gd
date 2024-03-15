c8fbcfafb6d693eb64f820d5b1ca73a5b5bf8a4c
Lucasdelpiero <lucasdelpiero98@gmail.com>
2024-03-14 23:18:46 -0300

    Function to improve relationship

--- Objects/Campaign/world/diplomacy_manager.gd
+++ Objects/Campaign/world/diplomacy_manager.gd
@@ -4,7 +4,7 @@ extends Node
 # This node manages the relationships between all the Nations
 # Will create nodes as children to store data for current wars
 
-@export var nation_group : Node 
+@export var nation_group : Node
 
 # Cached nations so it doesnt need to be called every time we want to update something
 # It will need to be updated every time a Nation gets created or destroyed
@@ -23,10 +23,34 @@ func _ready() -> void:
 		var new_diplomacy_nation := DiplomacyNation.new()
 		new_diplomacy_nation.initialize(nation)
 		diplomacy_nations_temp.push_back(new_diplomacy_nation)
-	
+
 	diplomacy_nations.assign(diplomacy_nations_temp)
 	# Calculate the starting relationship with the others
 	for nation in diplomacy_nations:
 		nation.calculate_relationships(nations)
+	
+	# TEST
+	for nation in diplomacy_nations:
+		print(nation.NATION_TAG)
+		print(nation.relationships)
+		if nation.NATION_TAG != "ROME":
+			improve_relationship("ROME",nation.NATION_TAG)
+		print(nation.NATION_TAG)
+		print(nation.relationships)
+	# TEST
+	
 
+# Uses the NATION_TAG string to find the diplomacy nations and improve their relationship
+func improve_relationship(sender: String, reciever: String) -> void:
+	var diplomacy_nation_tags : Array = diplomacy_nations.map(func(el: DiplomacyNation) -> String: return el.NATION_TAG)
+	var sender_position : int = diplomacy_nation_tags.find(sender)
+	var reciever_position : int = diplomacy_nation_tags.find(reciever)
+	if sender_position == 1 or reciever_position == -1:
+		push_error("Sender or reciever couldnt be found on diplomacy list")
+		return
+	# TEST
+	var improvement : int = 55
+	diplomacy_nations[reciever_position].improve_relationship_with(sender, improvement)
+	# TEST
+	pass
 
--- Objects/Campaign/world/diplomacy_nation.gd
+++ Objects/Campaign/world/diplomacy_nation.gd
@@ -19,11 +19,28 @@ func initialize(nation : Nation) -> void:
 func calculate_relationships(nations : Array[Nation]) -> void:
 	relationships.clear()
 	for nation in nations:
+		# Skip adding themselves to the relationship list
+		if nation.NATION_TAG == NATION_TAG:
+			continue
+		
 		var relations : int = 0
 		if nation.culture == culture:
 			relations += 25
 		relationships.push_back([nation.NATION_TAG, relations])
-	#print(NATION_TAG)
-	#print(relationships)
+
 	
+	pass
+
+
+func improve_relationship_with(nation_tag : String, amount: int) -> void:
+	var nation_tag_pos : int = relationships.map(func(el: Array) -> String: return el[0]).find(nation_tag) # TODO change this unsafe shit
+	if nation_tag_pos == -1:
+		push_error("Couldnt find the nation to improve relationship with")
+		return
+	# TEST
+	var new_value : int = relationships[nation_tag_pos][1] + amount
+	relationships[nation_tag_pos] = [nation_tag , new_value]
+	relationships[2][0] = "ALFONSO"
+	relationships[2][1] = 69
+	# TEST
 	pass
