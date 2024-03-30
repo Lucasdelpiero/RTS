bb2b00714285da80adb7fb914366914d9f678792
Lucasdelpiero <lucasdelpiero98@gmail.com>
2024-03-30 10:46:24 -0300

    Revert "fuck"
    This reverts commit aac80c5e413d0387415ca9f1aa730b1092c61bc1.

--- Objects/Campaign/world/province.gd
+++ Objects/Campaign/world/province.gd
@@ -4,6 +4,7 @@ extends Polygon2D
 
 @onready var inside_color : Color = Color(1.0, 1.0, 1.0) : set = set_color_inside
 @onready var outside_color : Color = Color(0.0, 0.0, 0.0)
+@export_color_no_alpha var outLine : Color = Color(0, 0, 0) : set = set_color_border
 @export_color_no_alpha var outline_color : Color = Color(0, 0, 0) : set = set_color_border
 @export_range(1, 20, 0.1) var width : float = 2.0 : set = set_width
 @onready var border : Line2D = %Border
@@ -71,8 +72,8 @@ signal sg_resources_generated(data : Production) # sent to the nation, needs to 
 
 func _ready() -> void:
 	inside_color = color
-	outside_color = outline_color
-	outline_color = outline_color
+	outside_color = outLine
+	outline_color = outLine
 	await get_tree().create_timer(1).timeout
 #	mouseDetectorCollition.shape.points = []
 	var poly : PackedVector2Array = get_polygon()
@@ -97,7 +98,7 @@ func _draw() -> void:
 		border.add_point(polygon[1]) # Closes the line from the end point to the start point
 	
 	border.width = width
-	border.default_color = outline_color
+	border.default_color = outLine
 
 func _input(_event : InputEvent) -> void:
 	pass
@@ -114,7 +115,7 @@ func set_color_inside(col: Color) -> void:
 	queue_redraw()
 
 func set_color_border(col : Color) -> void:
-	outline_color = col
+	outLine = col
 	queue_redraw()
 
 func set_width(new_width : float) -> void:
@@ -144,7 +145,7 @@ func get_connections() -> void:
 func update_to_nation_color() -> void:
 	for nation  in world.nations as Array[Nation]:
 		if nation.NATION_TAG == str(self.ownership):
-			self.outline_color = nation.nationOutline
+			self.outLine = nation.nationOutline
 			self.color = nation.nationColor
 			self.inside_color = nation.nationColor
 			self.outside_color = nation.nationOutline
@@ -165,23 +166,23 @@ func set_map_type_shown(type : String) -> void:
 	match type:
 		"political":
 			color = inside_color
-			border.default_color = outline_color
+			border.default_color = outLine
 			self_modulate.a = 1.0
 			border.self_modulate.a = 1.0
-			outline_color = outside_color
+			outLine = outside_color
 			
 		"terrain":
 			var new_color : Color = map_colors.get_terrain_color(terrain_type)
 			color = new_color
-			outline_color = new_color
+			outLine = new_color
 		"religion":
 			var new_color : Color = map_colors.get_religion_color(religion)
 			color = new_color
-			outline_color = new_color
+			outLine = new_color
 		"culture":
 			var new_color : Color = map_colors.get_culture_color(culture)
 			color = new_color
-			outline_color = new_color
+			outLine = new_color
 		_:
 			return
 	
