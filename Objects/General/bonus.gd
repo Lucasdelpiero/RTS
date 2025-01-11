ffefbfb2972340ce457eaf7308d098951eba1d7a
Lucasdelpiero <lucasdelpiero98@gmail.com>
2025-01-11 18:28:47 -0300

    Revert "fail trying to refactor the  bonuses"
    This reverts commit bade8898a5f1bc155f611e230b3c4342ca944534.

--- Objects/General/bonus.gd
+++ Objects/General/bonus.gd
@@ -1,21 +1,21 @@
 class_name Bonus
 extends Resource
 
-#@export_enum(
-	#BONUS_INCOME,
-	#BONUS_MANPOWER,
-	#BONUS_LOYALTY,
-	#BONUS_RELIGION_CONVERSION
-	#) var type_produced : String = BONUS_INCOME
-#@export_range(-100, 100, 5) var multiplier_bonus : int = 0
-#
-#
-#const BONUS_INCOME = "bonus_income"
-#const BONUS_MANPOWER = "bonus_manpower"
-#const BONUS_LOYALTY = "bonus_loyalty"
-#const BONUS_RELIGION_CONVERSION = "bonus_religion_conversion"
+@export_enum(
+	BONUS_INCOME,
+	BONUS_MANPOWER,
+	BONUS_LOYALTY,
+	BONUS_RELIGION_CONVERSION
+	) var type_produced : String = BONUS_INCOME
+@export_range(-100, 100, 5) var multiplier_bonus : int = 0
 
-#func copy_bonus(bonus : Bonus) -> Bonus:
-	#type_produced = bonus.type_produced
-	#multiplier_bonus = bonus.multiplier_bonus
-	#return self
+
+const BONUS_INCOME = "bonus_income"
+const BONUS_MANPOWER = "bonus_manpower"
+const BONUS_LOYALTY = "bonus_loyalty"
+const BONUS_RELIGION_CONVERSION = "bonus_religion_conversion"
+
+func copy_bonus(bonus : Bonus) -> Bonus:
+	type_produced = bonus.type_produced
+	multiplier_bonus = bonus.multiplier_bonus
+	return self
--- Objects/General/bonuses/bonus_max_population.gd
+++
@@ -1,4 +0,0 @@
-extends Bonus
-class_name BonusPopulation
-
-@export_range(100, 100_000, 100) var incremented_by : int = 100
