0cb715748fb83fbf6001cb9202dbc5bdcc905342
Lucasdelpiero <lucasdelpiero98@gmail.com>
2025-02-27 18:56:05 -0300

    Revert "States separated in movement/actions"
    This reverts commit d9b0c48201c92fce0a9a5a23fa576f182e45eead.

--- Objects/Battle/Units/Components/States/Movement/state_unit_movement.gd
+++
@@ -1,5 +0,0 @@
-class_name StateUnitMovement
-extends Node
-
-@export var unit_owner : Unit = null
-var state_machine : StateMachineUnit = null
---
+++ Objects/Battle/Units/Components/States/state_unit.gd
@@ -0,0 +1,14 @@
+class_name StateUnit
+extends Node
+
+@export var unit_owner : Unit = null
+var state_machine : StateMachineUnit = null
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
