f5c47e13e41f389f4da6fd02e47f18ee721aed3d
Lucasdelpiero <lucasdelpiero98@gmail.com>
2025-02-27 19:04:16 -0300

    Reapply "States separated in movement/actions"
    This reverts commit 0cb715748fb83fbf6001cb9202dbc5bdcc905342.

---
+++ Objects/Battle/Units/Components/States/Actions/state_unit_action.gd
@@ -0,0 +1,5 @@
+class_name StateUnitAction
+extends Node
+
+@export var unit_owner : Unit = null 
+var state_machine : StateMachineUnit = null
--- Objects/Battle/Units/Components/States/state_machine_unit.gd
+++ Objects/Battle/Units/Components/States/state_machine_unit.gd
@@ -2,9 +2,18 @@ class_name StateMachineUnit
 extends Node
 
 @export var unit_owner : Unit = null 
-var current_state : StateUnit = null
-var states : Array[StateUnit] = []
 
+# The state machine separates the movement state and the action states, so an unit
+# doesnt have to choose between moving and firing arrows, but can do both at the same time
+# without having to do spaghetti code in the process
+
+@onready var states_movement : Node = %StateMovement as Node
+var current_state_movement : StateUnitMovement = null
+var states_movement_list : Array[StateUnitMovement] = []
+
+@onready var states_action : Node = %StateAction as Node
+var current_state_action : StateUnitAction = null
+var states_action_list : Array[StateUnitAction]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,7 +22,14 @@ func _ready() -> void:
 	if unit_owner == null:
 		push_error("State machine couldnt find the unit that own the state machine")
 		return
-		
-	for child in get_children() as Array[StateUnit]:
+	
+	states_movement_list.assign(states_movement.get_children())
+	states_action_list.assign(states_action.get_children())
+	
+	for child in states_movement_list:
+		child.unit_owner = unit_owner
+		child.state_machine = self
+	
+	for child in states_action_list:
 		child.unit_owner = unit_owner
 		child.state_machine = self
